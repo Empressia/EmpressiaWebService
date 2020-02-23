5509cebf46793df5ad0d3368febb4bcc5ca33ecd
すふぃあ <sphere.soffie@gmail.com>
2020-02-23 21:18:00 +0900

    initial sources.

---
+++ EmpressiaWebService/src/main/java/jp/empressia/enterprise/ws/JSONParam.java
@@ -0,0 +1,21 @@
+package jp.empressia.enterprise.ws;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * WebServiceでJSONの値をバインドするためのアノテーションです。
+ * @author すふぃあ
+ */
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface JSONParam {
+
+    /** 目的の値までの名前の配列です。 */
+    String[] value() default "";
+
+}
